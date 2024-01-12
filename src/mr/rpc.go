7f14fa68d724caf160ffdbb0e9c11b9a35b40bfb
Hadi(SystopiaMac) <sinaee.hadi@gmail.com>
2024-01-11 20:10:52 -0800

    remove comments not related to UBC

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -28,8 +28,6 @@ type ExampleReply struct {
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
-// Can't use the current directory since
-// Athena AFS doesn't support UNIX-domain sockets.
 func coordinatorSock() string {
 	s := "/var/tmp/5840-mr-"
 	s += strconv.Itoa(os.Getuid())
